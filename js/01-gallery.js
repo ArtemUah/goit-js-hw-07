73faac8974a2f100d3bb11745c6c3d98ced3175c
Artem Shukatka <artem.shukatka@gmail.com>
2023-01-21 17:14:09 +0200

    for mentor 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
-const galleryMarkup = galleryItems.map(item => {
-  return  `<div class="gallery__item">
+const galleryMarkup = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
@@ -14,23 +15,27 @@ const galleryMarkup = galleryItems.map(item => {
     />
   </a>
 </div>
-    `
-}).join('');
+    `;
+  })
+  .join("");
 
 gallery.innerHTML = galleryMarkup;
 
-gallery.addEventListener('click', event => {
-    if (event.target.nodeName === 'IMG') {
-        event.preventDefault();
-        const lightBox = basicLightbox.create(`<img width="800" height="600" src="${event.target.dataset.source}">`, {
-            onShow: (lightBox) => {
-                document.addEventListener('keydown', event => {
-                    if (event.code === 'Escape') {
-                        lightBox.close();
-                    }
-                })
-                },
-        });
-        lightBox.show();
-    }
+gallery.addEventListener("click", (event) => {
+  if (event.target.nodeName === "IMG") {
+    event.preventDefault();
+    const lightBox = basicLightbox.create(
+      `<img width="800" height="600" src="${event.target.dataset.source}">`,
+      {
+        onShow: (lightBox) => {
+          document.addEventListener("keydown", (event) => {
+            if (event.code === "Escape") {
+              lightBox.close();
+            }
+          });
+        },
+      }
+    );
+    lightBox.show();
+  }
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,12 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery')
+const gallery = document.querySelector(".gallery");
 
-const galleryMarkup = galleryItems.map(item => {
-  return  `
+const galleryMarkup = galleryItems
+  .map((item) => {
+    return `
   <a class="gallery__item" href="${item.original}">
     <img
       class="gallery__image"
@@ -14,9 +15,17 @@ const galleryMarkup = galleryItems.map(item => {
       alt="${item.description}"
     />
   </a>
-    `
-}).join('');
+    `;
+  })
+  .join("");
 
 gallery.innerHTML = galleryMarkup;
 
-new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250, widthRatio: 0.5, heightRatio: 0.5, overlayOpacity: 0.5});
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  widthRatio: 0.5,
+  heightRatio: 0.5,
+  overlayOpacity: 0.5,
+  showCounter: false,
+});
